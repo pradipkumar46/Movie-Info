7d66a48307d9f3a7bf9b0370ff11bc89c3cf5718
pradipkumar <pradipkumar.java@gmail.com>
2019-07-16 23:53:14 +0530

    MovieInfo-Service

---
+++ src/main/java/com/spring/MoviInfoApplication.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MoviInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviInfoApplication.class, args);
+	}
+
+}
